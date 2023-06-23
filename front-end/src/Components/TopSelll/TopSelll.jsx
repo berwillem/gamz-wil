3590e1dc6330fe007a7487589a8d1113dfb28e01
berchiche willem <berwillempro@gmail.com>
2023-06-23 23:24:37 +0100

    fixe

--- front-end/src/Components/TopSelll/TopSelll.jsx
+++ front-end/src/Components/TopSelll/TopSelll.jsx
@@ -5,96 +5,56 @@ import "./TopSelll.css";
 import { Box } from "../../Data/Box";
 import TopSelBox from "../TopSelBox/TopSelBox";
 import axios from "axios";
-
 const baseURL = import.meta.env.VITE_BASE_URL;
+
 const boxSlice = Box.slice(0, 3);
 
 function TopSelll() {
+  //state
   const [postsConsole, setPostsConsole] = useState([]);
   const [postsTelephone, setPostsTelephone] = useState([]);
   const [postsInformatique, setPostsInformatique] = useState([]);
-
-  useEffect(() => {
-    // Fetch posts for category "console"
-    axios
-      .get(baseURL + "/post/category/644b93f21b7633709d2db121")
-      .then((response) => {
-        setPostsConsole(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
-  useEffect(() => {
-    // Fetch posts for category "informatique"
-    axios
-      .get(baseURL + "/post/category/644b907d1b7633709d2db0fa")
-      .then((response) => {
-        setPostsInformatique(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
- function TopSelll() {
-  //state
-const [postsConsole, setPostsConsole] = useState([]);
-const [postsTelephone, setPostsTelephone] = useState([]);
-const [postsInformatique, setPostsInformatique] = useState([]);
-// variable 
-const boxSlice1 = shuffleArray(postsConsole).slice(0,3)
-const boxSlice2 = shuffleArray(postsInformatique).slice(0,3)
-const boxSlice3 = shuffleArray(postsTelephone).slice(0,3)
-// api call
-useEffect(() => {
-   
-  axios.get(baseURL+`/post/category/644b93f21b7633709d2db121` )
-    .then(response => {
-      // Traiter les données de la réponse
-      setPostsConsole(response.data)
-      
-   
-    })
-    .catch(error => {
-      // Gérer les erreurs
-      console.error(error);
-    });
-}, []);
-useEffect(() => {
-   
-  axios.get(baseURL+`/post/category/644b907d1b7633709d2db0fa` )
-    .then(response => {
-      // Traiter les données de la réponse
-      setPostsInformatique(response.data)
-      
-   
-    })
-    .catch(error => {
-      // Gérer les erreurs
-      console.error(error);
-    });
-}, []);
-useEffect(() => {
-   
-  axios.get(baseURL+`/post/category/644b97091b7633709d2db1d8` )
-    .then(response => {
-      // Traiter les données de la réponse
-      setPostsTelephone(response.data)
-      
-   
-    })
-    .catch(error => {
-      // Gérer les erreurs
-      console.error(error);
-    });
-}, []);
-
-
+  // variable
   const boxSlice1 = shuffleArray(postsConsole).slice(0, 3);
   const boxSlice2 = shuffleArray(postsInformatique).slice(0, 3);
   const boxSlice3 = shuffleArray(postsTelephone).slice(0, 3);
+  // api call
+  useEffect(() => {
+    axios
+      .get(baseURL + `/post/category/644b93f21b7633709d2db121`)
+      .then((response) => {
+        // Traiter les données de la réponse
+        setPostsConsole(response.data);
+      })
+      .catch((error) => {
+        // Gérer les erreurs
+        console.error(error);
+      });
+  }, []);
+  useEffect(() => {
+    axios
+      .get(baseURL + `/post/category/644b907d1b7633709d2db0fa`)
+      .then((response) => {
+        // Traiter les données de la réponse
+        setPostsInformatique(response.data);
+      })
+      .catch((error) => {
+        // Gérer les erreurs
+        console.error(error);
+      });
+  }, []);
+  useEffect(() => {
+    axios
+      .get(baseURL + `/post/category/644b97091b7633709d2db1d8`)
+      .then((response) => {
+        // Traiter les données de la réponse
+        setPostsTelephone(response.data);
+      })
+      .catch((error) => {
+        // Gérer les erreurs
+        console.error(error);
+      });
+  }, []);
 
   function shuffleArray(array) {
     const shuffledArray = [...array];
@@ -107,10 +67,9 @@ useEffect(() => {
     }
     return shuffledArray;
   }
-
   gsap.registerPlugin(ScrollTrigger);
-  const cardContainer = useRef();
 
+  const cardContainer = useRef();
   useEffect(() => {
     gsap.to(cardContainer.current, {
       x: 0,
@@ -121,39 +80,38 @@ useEffect(() => {
       scrollTrigger: cardContainer.current,
     });
   }, []);
-
   return (
     <>
-      {boxSlice1.length === 0 ||
-      boxSlice2.length === 0 ||
-      boxSlice3.length === 0 ? (
+      {boxSlice1.length == 0 ||
+      boxSlice2.length == 0 ||
+      boxSlice3.length == 0 ? (
         <div className="noPost">
-          <h2>NO POSTS YET</h2>
+          <h2> PAS DE POST ENCORE </h2>
         </div>
       ) : (
         <div className="topSell-container">
           <div className="top-Sell-title">
-            <h4>Other listings</h4>
+            <h4>Autres annonces</h4>
           </div>
           <div className="topSell-content" ref={cardContainer}>
             <div className="grid">
-              {boxSlice1.map((item, key) => (
-                <TopSelBox key={key} i={item} />
+              {boxSlice1.map((i, key) => (
+                <TopSelBox i={i} />
               ))}
             </div>
             <div className="grid">
-              {boxSlice2.map((item, key) => (
-                <TopSelBox key={key} i={item} />
+              {boxSlice2.map((i, key) => (
+                <TopSelBox i={i} />
               ))}
             </div>
             <div className="grid">
-              {boxSlice3.map((item, key) => (
-                <TopSelBox key={key} i={item} />
+              {boxSlice3.map((i, key) => (
+                <TopSelBox i={i} />
               ))}
             </div>
           </div>
         </div>
-      )}
+      )}{" "}
     </>
   );
 }
