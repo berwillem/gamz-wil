41b9ef70ea5fb5f2139f9c38b460ce9ea447a403
berchiche willem <berwillempro@gmail.com>
2023-07-15 18:37:42 +0100

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 const app = express();
 const mongoose = require("./config/db");
 const authRoute = require("./app/routes/AuthRoute");
@@ -21,9 +23,19 @@ app.use("/api/v1/post", postRoute);
 app.use("/api/v1/pub", PubRoute);
 app.use("/api/v1/user", UserRoute);
 
-//*  serveur handling :::
+//* server handling :::
 
 const PORT = process.env.PORTS || 5000;
-app.listen(5000, () => {
-  console.log(`Express server is running on PORT ${PORT}...`);
+
+const privateKeyPath = "/etc/letsencrypt/live/gamz-dz.com/privkey.pem";
+const certificatePath = "/etc/letsencrypt/live/gamz-dz.com/fullchain.pem";
+
+const privateKey = fs.readFileSync(privateKeyPath, "utf8");
+const certificate = fs.readFileSync(certificatePath, "utf8");
+const credentials = { key: privateKey, cert: certificate };
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(PORT, () => {
+  console.log(`Express server is running on HTTPS port ${PORT}...`);
 });
