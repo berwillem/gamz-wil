635d590bf3480f88b550b026f55d79bf22194de6
Wassim <512.amosimo@gmail.com>
2024-06-30 15:08:51 +0100

    Pagination

--- front-end/src/services/Posts.js
+++ front-end/src/services/Posts.js
@@ -9,16 +9,16 @@ export const getPost = async (id) => {
   return axios.get(`${baseURL}/post/${id}`);
 };
 
-export const getPosts = async () => {
-  return axios.get(`${baseURL}/post`);
+export const getPosts = async (page) => {
+  return axios.get(`${baseURL}/post`, { params: { page } });
 };
 
-export const getPostsByCategory = async (categoryId) => {
-  return axios.get(`${baseURL}/post/category/${categoryId}`);
+export const getPostsByCategory = async (categoryId, page) => {
+  return axios.get(`${baseURL}/post/category/${categoryId}`, { params: { page } });
 };
 
-export const getPostsBySubcategory = async (subcategoryId) => {
-  return axios.get(`${baseURL}/post/subcategory/${subcategoryId}`);
+export const getPostsBySubcategory = async (subcategoryId, page) => {
+  return axios.get(`${baseURL}/post/subcategory/${subcategoryId}`, { params: { page } });
 };
 export const getPostsBycategoryCount = async () => {
   return axios.get(`${baseURL}/post/category/count`);
