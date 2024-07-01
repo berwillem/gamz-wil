4fa6ea1e08fab55ebadb6061d59d4e6de74fd4e0
berchiche willem <berwillempro@gmail.com>
2024-07-01 14:48:24 +0100

    pub-no-cach

--- backend/app/controllers/pubMobilController.js
+++ backend/app/controllers/pubMobilController.js
@@ -58,3 +58,14 @@ exports.getPubMobil = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+exports.getPubMobilNoCach = async (req, res) => {
+  try {
+    const pub = await pubmobil.findOne();
+    if (!pub) {
+      return res.status(404).json({ message: "Pub not found" });
+    }
+    res.status(200).json(pub);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
