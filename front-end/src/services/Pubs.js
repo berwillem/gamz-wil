4294519286fc60281a162811e27f6ca157fa43d7
Wassim <512.amosimo@gmail.com>
2024-07-02 10:36:07 +0100

    SidePubs

--- front-end/src/services/Pubs.js
+++ front-end/src/services/Pubs.js
@@ -12,3 +12,13 @@ export const createPub = async (formData, sessionId) => {
     headers: { "session-id": sessionId },
   });
 };
+
+export const createSidePub = async (formData, sessionId) => {
+  return axios.post(baseURL + "/pub/side", formData, {
+    headers: { "session-id": sessionId },
+  });
+}
+
+export const getSidePub = async (num) => {
+  return axios.get(baseURL + `/pub/side/${num}`);
+}
