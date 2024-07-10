8711539ad9b8aa2d6b4e09bc6b7a2c43b5103f19
berchiche willem <berwillempro@gmail.com>
2024-07-10 23:03:36 +0100

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 const app = express();
 const mongoose = require("./config/db");
 const session = require("express-session");
@@ -7,9 +9,9 @@ const CategoryRoute = require("./app/routes/CategoryRoute");
 const postRoute = require("./app/routes/PostRoute");
 const UserRoute = require("./app/routes/UserRoute");
 const PubRoute = require("./app/routes/PubRoute");
-
 const cors = require("cors");
 require("dotenv").config();
+app.use(cors());
 app.use(
   session({
     secret: process.env.CLIENT_SECRET,
@@ -17,7 +19,6 @@ app.use(
     saveUninitialized: false,
   })
 );
-app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
@@ -29,9 +30,19 @@ app.use("/api/v1/post", postRoute);
 app.use("/api/v1/pub", PubRoute);
 app.use("/api/v1/user", UserRoute);
 
-//*  serveur handling :::
+//* server handling :::
 
 const PORT = process.env.PORTS || 5000;
-app.listen(5000, () => {
-  console.log(`Express server is running on PORT ${PORT}...`);
+
+const privateKeyPath = "/etc/letsencrypt/live/gamz-dz.com/privkey.pem";
+const certificatePath = "/etc/letsencrypt/live/gamz-dz.com/fullchain.pem";
+
+const privateKey = fs.readFileSync(privateKeyPath, "utf8");
+const certificate = fs.readFileSync(certificatePath, "utf8");
+const credentials = { key: privateKey, cert: certificate };
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(PORT, () => {
+  console.log(`Express server is running on HTTPS port ${PORT}...`);
 });
