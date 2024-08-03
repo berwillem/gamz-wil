58942d7f9b44069ed85a1e32676cb4f04fb19f16
Wassim <512.amosimo@gmail.com>
2024-08-03 17:37:52 +0100

    image upload fix

--- backend/app/middlewares/uploadConfig.js
+++ backend/app/middlewares/uploadConfig.js
@@ -1,15 +1,23 @@
 const multer = require("multer");
-const cloudinary = require("cloudinary").v2;
+// const cloudinary = require("cloudinary").v2;
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_KEY,
-  api_secret: process.env.CLOUD_KEY_SECRET,
-});
+// cloudinary.config({
+//   cloud_name: process.env.CLOUD_NAME,
+//   api_key: process.env.CLOUD_KEY,
+//   api_secret: process.env.CLOUD_KEY_SECRET,
+// });
 // change the config
 const storage = multer.diskStorage({
   limits: {
     fileSize: 50  * 1024 * 1024, 
+  },
+  destination: (req, file, cb) => {
+    // const fieldName = file.fieldname; // get the form data field name
+    const destinationFolder = `/var/www/html/images/`; // set the destination folder based on the field name
+    cb(null, destinationFolder);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '.' + file.originalname.split(".").at(-1));
   },
 });
 
